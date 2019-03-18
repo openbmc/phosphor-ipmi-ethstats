17e24739edbb2e4b10e4544b11ab91c9c6e92886
Patrick Venture <venture@google.com>
2019-03-18 11:14:16 -0700

    for injection, add handler
    Add a handler to enable injection for testing.
    
    Change-Id: I991849e6df2c3e74f59145a966048fc6825560db
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ handler.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace ethstats
+{
+
+struct StatRequest
+{
+    StatRequest(const std::string& name, const std::string& field) :
+        ifName(name), field(field)
+    {
+    }
+    std::string ifName;
+    std::string field;
+};
+
+class EthStatsInterface
+{
+  public:
+    virtual ~EthStatsInterface() = default;
+
+    /** Given an ifname and a statistic, validate both.
+     *
+     * @param[in] request - the interface name and statistics field
+     * @return true if both valid, false otherwise.
+     */
+    virtual bool validIfNameAndField(const StatRequest& request) const = 0;
+
+    /** Given an ifname and a statistic, return the value.
+     *
+     * @param[in] request - the interface name and statistics field
+     * @return the value of that statistic for that interface.
+     */
+    virtual std::uint64_t readStatistic(const StatRequest& request) const = 0;
+};
+
+class EthStats : public EthStatsInterface
+{
+  public:
+    EthStats() = default;
+    ~EthStats() = default;
+
+    bool validIfNameAndField(const StatRequest& request) const override;
+    std::uint64_t readStatistic(const StatRequest& request) const override;
+};
+
+extern EthStats handler;
+
+} // namespace ethstats
